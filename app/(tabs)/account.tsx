54974799766b4609ca13aaaac9caff3c3e24350d
pegaislethal <ghimirerohit21@gmail.com>
2024-10-25 20:28:40 +0545

    Enhance account page: added new feature

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,30 +1,76 @@
-import { Text,Image,Pressable, useColorScheme } from "react-native";
+import {
+  Text,
+  Image,
+  Pressable,
+  useColorScheme,
+  View,
+  StyleSheet,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Link } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import { Colors } from "@/constants/Colors";
+import { ThemedText } from "@/components/ThemedText";
 
 export default function account() {
+  const theme = useColorScheme() ?? "light";
+
   return (
-    <SafeAreaView>
-     <Text>Account Page</Text>
-     <Link href={"/accountinfo"}>
-     <Text>Account Information</Text></Link>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Header />
+      <View style={{flex:1}}>
+        <AuthButton
+          labels={"Sign In"}
+          icons={
+            <Ionicons
+              name={"logo-google"}
+              size={24}
+              color={theme === "light" ? Colors.light.icon : Colors.dark.icon}
+            />      
+          }
+        />
+
+<AuthButton
+          labels={"Sign In"}
+          icons={
+            <Ionicons
+              name={"logo-apple"}
+              size={24}
+              color={theme === "light" ? Colors.light.icon : Colors.dark.icon}
+            />      
+          }
+        />
+    
+
+
+      </View>
+
+      <View style={{}}>
+        <Link href={"/accountinfo"}>
+          <Text>Account Information</Text>
+        </Link>
+      </View>
     </SafeAreaView>
   );
 }
 
-function authButton({ labels, icons }: { 
-  labels: string;
-  icons: any })
-{
+function Header() {
+  return (
+    <View style={styles.topBar}>
+      <ThemedText style={styles.textBig}>Panels</ThemedText>
+      <ThemedText>Sign in to save your data</ThemedText>
+    </View>
+  );
+}
+
+function AuthButton({ labels, icons }: { labels: string; icons: any }) {
   const theme = useColorScheme() ?? "light";
   return (
     <Pressable
       style={{
         backgroundColor: "black",
         padding: 10,
-        marginHorizontal: 30,
+        marginHorizontal: 40,
         paddingVertical: 15,
         justifyContent: "center",
         flexDirection: "row",
@@ -32,12 +78,27 @@ function authButton({ labels, icons }: {
       }}
     >
       {icons}
-      <Text style={{
-        fontSize:20,
-        color:"white",
-        fontWeight:"600",
-      }}>{labels}</Text>
+      <Text
+        style={{
+          fontSize: 20,
+          color: "white",
+          fontWeight: "600",
+        }}
+      >
+        {labels}
+      </Text>
     </Pressable>
   );
 }
-  
+
+const styles = StyleSheet.create({
+  textBig: {
+    fontSize: 20,
+    fontWeight: 600,
+  },
+  topBar: {
+    padding: 20,
+  },
+  bottom:{
+  }
+});
